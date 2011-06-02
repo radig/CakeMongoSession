81f8791efa652a3109d06527c417866ef8a07564
Cauan Cabral <cauan@radig.com.br>
2011-06-02 16:53:34 -0400

    Correção no README
    Atualização do arquivo de configuração

--- config/cake_mongo_session.php
+++ config/cake_mongo_session.php
@@ -1,1 +1,14 @@
 <?php
+//Configure::write('MongoSession', array(
+//	'servers' => array(
+//		array(
+//			'host' => 'localhost',
+//			'port' => 27017,
+//			'username' => 'dotti',
+//			'password' => 'senha'
+//			)
+//		),
+//		'database' => 'radig_sessions',
+//		'collection' => 'sessions'
+//	)
+//);
